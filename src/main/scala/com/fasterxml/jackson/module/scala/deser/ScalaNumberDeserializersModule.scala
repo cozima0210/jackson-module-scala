a6f7420bdddff1fb7a4e67ef13897e4b1d528c8c
cozima <cozima@zozo.com>
2020-07-28 13:31:59 +0900

    Fix to use getNumberValue

--- src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/deser/ScalaNumberDeserializersModule.scala
@@ -17,7 +17,7 @@ private abstract class BigNumberDeserializer[T >: Null : ClassTag](creator: (Str
   override def deserialize(jp: JsonParser, ctxt: DeserializationContext): T = {
     val t = jp.getCurrentToken
     t match {
-      case VALUE_NUMBER_INT | VALUE_NUMBER_FLOAT => creator(jp.getText.trim)
+      case VALUE_NUMBER_INT | VALUE_NUMBER_FLOAT => creator(jp.getNumberValue.toString)
       case VALUE_STRING =>
         val text = jp.getText.trim
         if (text.isEmpty) None.orNull else try {
